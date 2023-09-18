365eefec391640f63034a9d7fe138ba429a30068
jabranemohamed <jabranemohamed@gmail.com>
2023-09-18 12:46:55 +0200

    first init

--- src/main/java/io/adservio/crm/utils/SmsUtils.java
+++ src/main/java/io/adservio/crm/utils/SmsUtils.java
@@ -8,9 +8,9 @@ import static com.twilio.rest.api.v2010.account.Message.creator;
 
 
 public class SmsUtils {
-    public static final String FROM_NUMBER = "0015177422073";
-    public static final String SID_KEY = "ACb2ef457dffda7986af7caf0a9170a74d";
-    public static final String TOKEN_KEY = "c9104ace7ad40d48abafbcb86d8ea45f";
+    public static final String FROM_NUMBER = "XXXXXXX";
+    public static final String SID_KEY = "XXXXXXX";
+    public static final String TOKEN_KEY = "XXXXXX";
 
     public static void sendSMS(String to, String messageBody) {
         Twilio.init(SID_KEY, TOKEN_KEY);
